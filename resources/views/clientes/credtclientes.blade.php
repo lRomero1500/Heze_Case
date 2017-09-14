96098b88111846329faf29201bb61f197c7275da
luisd <lromero@arciait.com>
2017-09-14 11:22:44 -0500

    dasds

---
+++ resources/views/clientes/credtclientes.blade.php
@@ -0,0 +1,23 @@
+
+<div class="contentFormulario2Colomnas">
+    <div class="tituloTabla">
+        <h2>Datos del cliente</h2>
+    </div>
+    <div class="contenedorDeCampos">
+        <div class="campoCorto"><h5>Nombre</h5>
+            <input type="text" value="" placeholder="">
+        </div>
+        <div class="campoCorto"><h5>Cargo</h5>
+            <input type="text" value="" placeholder="">
+        </div>
+        <div class="campoCorto"><h5>Correo</h5>
+            <input type="mail" value="" placeholder="">
+        </div>
+        <div class="campoCorto"><h5>Teléfono</h5>
+            <input type="text" value="" placeholder="">
+        </div>
+    </div>
+</div>
+<div class="btnGuardar" style="display: inline-block;">
+    <button style="color: #ffffff !important;"><i class="fa fa-floppy-o btnIconComun"></i><p> Guardar datos</p></button>
+</div>
