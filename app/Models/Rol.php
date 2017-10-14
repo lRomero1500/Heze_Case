e79e0e646b4b187af0f0a191470c4c7a79a43cb1
luisd <lromero@arciait.com>
2017-10-14 15:33:52 -0500

    Funcionalidad de validacion de formularios con jquery form validator

---
+++ app/Models/Rol.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model
+{
+    protected $primaryKey='cod_Rol';
+    protected $table='cotz_roles';
+    protected $fillable=['nom_Rol','cod_Companias'];
+    protected $hidden=['cod_Rol'];
+
+}
---
+++ app/Models/RolDetalle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RolDetalle extends Model
+{
+    protected $primaryKey='cod_Detale_Rol';
+    protected $table='cotz_detalle_roles';
+    protected $fillable=['cod_menu','permisos'];
+    protected $hidden=['cod_Detale_Rol','cod_Rol'];
+
+}
