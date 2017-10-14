c63c713cab083f7b756d4404284a4adbd6b2b849
Freddy Arcia <farcia@arciait.com>
2017-10-14 15:32:16 -0500

    Freddy

--- database/migrations/2017_10_08_231338_Alter_Menu_Add_Column_table.php
+++ database/migrations/2017_10_08_231338_Alter_Menu_Add_Column_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterMenuTable extends Migration
+class AlterMenuAddColumnTable extends Migration
 {
     /**
      * Run the migrations.
